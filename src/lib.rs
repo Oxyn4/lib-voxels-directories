d7a6d93561fc942060917ca48d7432f4baaf8b37
jacob <77414989+Oxyn4@users.noreply.github.com>
2025-04-12 20:55:53 +0100

    re-export voxels application crate

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 pub mod voxels;
-pub mod application;
 pub mod environment_variables;
 pub mod filesystem;
+
+use lib_voxels_application;
