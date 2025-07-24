de3c646a546ab96da05d959d4b9957345c2dccec
jacob <77414989+Oxyn4@users.noreply.github.com>
2025-07-24 16:44:07 +0100

    add license disclaimer into files

--- src/voxels/data.rs
+++ src/voxels/data.rs
@@ -1,19 +1,24 @@
+/*
+Copyright (C) 2025  Jacob Evans
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 use std::path::PathBuf;
 use crate::voxels::VoxelsDirectoryError;
 
 use super::voxels_xdg::data as base;
-
-use dbus::nonblock::{
-    SyncConnection,
-    Proxy
-};
-
-use std::sync::Arc;
-use std::time::Duration;
-use dbus_tokio::connection;
-use dbus_tokio::connection::IOResourceError;
-
-use tokio_util::sync::CancellationToken;
 
 use lib_voxels_application::application::application::Application;
 
--- src/voxels/mod.rs
+++ src/voxels/mod.rs
@@ -14,6 +14,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
+
 use voxels_xdg::xdg::BaseDirectoryError;
 
 #[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash)]
